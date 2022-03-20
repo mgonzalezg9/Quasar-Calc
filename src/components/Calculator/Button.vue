c84ec9c89f6da7eaa53e9a7f9abebb2ac1a49105
Manuel <mgonzalez4648@gmail.com>
2022-03-20 16:33:21 +0100

    App structure

---
+++ src/components/Calculator/Button.vue
@@ -0,0 +1,22 @@
+<template>
+  <q-btn
+    class="full-width text-h6"
+    text-color="white"
+    color="indigo"
+    @click="handleClick"
+  >
+    <slot>{{ fallbackContent }}</slot>
+  </q-btn>
+</template>
+
+<script setup lang="ts">
+const emit = defineEmits<{ (e: 'click'): void }>();
+
+const fallbackContent = '-';
+function handleClick() {
+  emit('click');
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ src/components/Calculator/index.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="row justify-center">
+    <div class="col-12 col-md-8">
+      <q-card>
+        <q-card-section class="bg-primary text-white">
+          <div class="text-h6">{{ title }}</div>
+        </q-card-section>
+
+        <q-card-section>
+          <div class="text-h5 text-grey-5 text-right">asdf</div>
+          <div class="text-h3 text-right">res</div>
+        </q-card-section>
+
+        <q-card-section class="row">
+          <calculator-button v-for="i in 10" :key="i" class="col-3">{{
+            i
+          }}</calculator-button>
+        </q-card-section>
+      </q-card>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, defineProps } from 'vue';
+import CalculatorButton from './Button.vue';
+
+export default defineComponent({
+  name: 'CalculatorPage',
+  components: {
+    CalculatorButton,
+  },
+  props: {
+    title: {
+      type: String,
+      default: 'Calculator',
+    },
+  },
+});
+</script>
+
+
